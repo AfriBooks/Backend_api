c3a0189553413d38254a8f9d6e7f6b930562616f
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-05-24 13:32:24 +0100

    created json, urlencoded with express

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express, {Request, Response, Application} from "express";
 
 const app = express();
 const port:number = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', (req:Request, res:Response )=>{
   res.send('Welcome to AfriBook API')
