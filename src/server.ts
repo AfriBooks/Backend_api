58c2ce07a6c5c99e5d84782b3aa1cbb9cb9e9cb7
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-05-24 13:27:29 +0100

    setup server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response, Application} from "express";
+
+const app = express();
+const port:number = 3000;
+
+app.get('/', (req:Request, res:Response )=>{
+  res.send('Welcome to AfriBook API')
+})
+
+app.listen(port, ()=>{
+  console.log(`App is listening on port ${port}`);
+  
+})
