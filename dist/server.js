7bb415a8ba159b7758cef5d8a2d9d759879475ea
saslamp <sadiqasg@gmail.com>
2022-06-07 14:17:20 +0100

    > merged main branch
    > modified delete controller

--- dist/server.js
+++ dist/server.js
@@ -8,7 +8,7 @@ var cors_1 = __importDefault(require("cors"));
 var cookie_parser_1 = __importDefault(require("cookie-parser"));
 var database_1 = __importDefault(require("./database"));
 var book_1 = __importDefault(require("./routes/book"));
-var user_1 = __importDefault(require("./routes/user"));
+var user_1 = require("./routes/user");
 (0, database_1["default"])()["catch"](function (err) { return console.error(err); });
 var app = (0, express_1["default"])();
 var port = 3000;
@@ -17,7 +17,7 @@ app.use(express_1["default"].json());
 app.use(express_1["default"].urlencoded({ extended: false }));
 app.use((0, cookie_parser_1["default"])());
 (0, book_1["default"])(app);
-(0, user_1["default"])(app);
+(0, user_1.userRoute)(app);
 app.get("/", function (req, res) {
     res.status(200).send("Welcome to AfriBook API");
 });
