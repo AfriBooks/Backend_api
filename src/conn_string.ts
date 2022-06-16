aea5013f589850a17083e6f83a3fefca10d8332b
saslamp <sadiqasg@gmail.com>
2022-06-16 12:51:29 +0100

    added conn_string to main branch

---
+++ src/conn_string.ts
@@ -0,0 +1,3 @@
+export const DEV_MONGO_URI = "mongodb://localhost:27017/afribook";
+export const MONGO_ATLAS_URI =
+    "mongodb+srv://afribook_admin:afribook_pwd@cluster0.ahoox.mongodb.net/?retryWrites=true&w=majority";
