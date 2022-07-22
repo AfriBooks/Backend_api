8ef186f1c99b64ca727381d5087277ac63302873
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-07-22 20:13:22 +0100

    created function to call google consent screen

--- src/ui/getGoogleUri.ts
+++ src/ui/getGoogleUri.ts
@@ -3,5 +3,25 @@
 const getGoogleUri = ()=>{
    const rootUrl = ''
 
-   const options
+   const options ={
+    redirect_url: process.env.PUBLIC_GOOGLE_OAUTH_REDIRECT_URL as string,
+    client_id: process.env.PUBLIC_GOOGLE_CLIENT_ID as string,
+    access_type: 'offline',
+    response_type: 'code',
+    prompt: 'consent',
+    scope: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email'
+    ].join(" ")
+   }
+
+   console.log({options});
+   
+
+   const qs = new URLSearchParams(options);
+
+   console.log({qs});
+   
+
+   return `${rootUrl}?${qs.toString()}`;
 }
