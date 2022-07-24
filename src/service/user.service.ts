ca42a6527fed08aab4fb45b5ad2e5d4a110d15a4
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-07-24 17:06:35 +0100

    complemented network request to fetch google auth tokens

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,6 +1,33 @@
+import axios, { Axios} from 'axios'
+import { env } from 'process';
+import config from ".../config";
+import qs from "qs"
+
+
+
+
+
 
 export const getGoogleOauthTokens = async ({code}:{code: string})=>{
 
     const url = 'http://oauth2.googleapis.com/token';
-    const values = ''
+    const values = {
+        code,
+        client_id: config.get('googleClientId'),
+        client_secret: config.get('googleClientSecret'),
+        redirect_url: config.get('googleOauthRedirectUrl'),
+        grant_type: "authorization_code"
+    };
+
+    try {
+        const res = await axios.post(url, qs.stringify(values),{
+           headers: {
+             'Content-Type': 'application/x-www-form-urlencoded'
+           } 
+        })
+        return res.data;
+    } catch (error: any) {
+        console.log(error, 'Failed to fetch Google Oauth Tokens');
+        throw new Error(error.message)
+    }
 }
