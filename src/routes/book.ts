595c38cfd60e5a8ec519a5f2001b712b52b29733
saslamp <sadiqasg@gmail.com>
2022-08-18 14:50:17 +0100

    bug fix and update

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -22,7 +22,7 @@ const bookRoutes = (app: Application) => {
     app.get("/books/categories/:category", getBooksByCategories);
     app.delete("/books/:id", verifyAuthToken, deleteBook);
     app.patch("/books/:id", verifyAuthToken, updateBook);
-    app.patch("/books/:id/reviews",verifyAuthToken, review);
+    app.patch("/books/:id/reviews", review);
     app.patch("/books/:id/reviews/:reviewId/reply", verifyAuthToken, reviewReply);
     app.get("/books/:id/reviews", getBookReviews);
     app.get('/users/:id/books', getBooksByUser);
